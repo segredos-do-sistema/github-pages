1f8244e41ce52e63d1052dbd01d21e33b84e40e3
segredos-do-sistema <136630163+segredos-do-sistema@users.noreply.github.com>
2023-07-24 21:47:35 -0300

    criando um site simples

---
+++ script.js
@@ -0,0 +1,77 @@
+// cadastre-se N1
+li1.addEventListener('mouseenter',entrou1)
+li1.addEventListener('mouseout',saiu1)
+
+
+function entrou1(){
+    
+    img1.src = "imagens/cadastro.png"
+    img1.style.display = "flex"
+    img1.style.flex.direction = "column"
+
+    
+}
+function saiu1(){
+    img1.src = ""
+}
+// Upload N2
+li2.addEventListener('mouseenter',entrou2)
+li2.addEventListener('mouseout',saiu2)
+
+function entrou2(){
+    img2.src = "imagens/repositorio.png"
+    img2.style.display = "flex"
+    img2.style.flex.direction = "column"
+}
+function saiu2(){
+    img2.src = ""
+}
+
+// Index N3
+li3.addEventListener('mouseenter',entrou3)
+li3.addEventListener('mouseout',saiu3)
+
+function entrou3(){
+    img3.src = "imagens/index.png"
+    img3.style.display = "flex"
+    img3.style.flex.direction = "column"
+}
+function saiu3(){
+    img3.src = ""
+}
+// settings N4
+li4.addEventListener('mouseenter',entrou4)
+li4.addEventListener('mouseout',saiu4)
+
+function entrou4(){
+    img4.src = "imagens/settings.png"
+    img4.style.display = "flex"
+    img4.style.flex.direction = "column"
+}
+function saiu4(){
+    img4.src = ""
+}
+// pages N5
+li5.addEventListener('mouseenter',entrou5)
+li5.addEventListener('mouseout',saiu5)
+
+function entrou5(){
+    img5.src = "imagens/pages.png"
+    img5.style.display = "flex"
+    img5.style.flex.direction = "column"
+}
+function saiu5(){
+    img5.src = ""
+}
+// main N5
+li6.addEventListener('mouseenter',entrou6)
+li6.addEventListener('mouseout',saiu6)
+
+function entrou6(){
+    img6.src = "imagens/main.png"
+    img6.style.display = "flex"
+    img6.style.flex.direction = "column"
+}
+function saiu6(){
+    img6.src = ""
+}
